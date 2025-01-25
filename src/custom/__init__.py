1b3c230e4374dca05f27e973ba4ff0c4b5490bc7
limitcantcode <limit@limit-cc.com>
2025-01-26 00:45:04 +0100

    Update to new jaison-grpc version and fix bugs
    Signed-off-by: limitcantcode <limit@limit-cc.com>

--- src/custom/__init__.py
+++ src/custom/__init__.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 '''
 Supported component type entrypoints
 
@@ -12,20 +15,42 @@ import os
 from .model import UnslothAIModel
 t2t_model = UnslothAIModel(os.getenv("MODEL"), os.getenv("IS_4_BIT")=='true')
 
-# For speech-to-text models
-def start_stt(audio: bytes) -> str:
-    raise NotImplementedError
+from jaison_grpc.common import STTComponentRequest, T2TComponentRequest, TTSGComponentRequest, TTSCComponentRequest
+async def request_unpacker(request_iterator):
+    async for request_o in request_iterator:
+        match request_o:
+            case STTComponentRequest():
+                yield request_o.audio, request_o.sample_rate, request_o.sample_width, request_o.channels
+            case T2TComponentRequest():
+                yield request_o.system_input, request_o.user_input
+            case TTSGComponentRequest():
+                yield request_o.content
+            case TTSCComponentRequest():
+                yield request_o.audio, request_o.sample_rate, request_o.sample_width, request_o.channels
+            case _:
+                raise Exception(f"Unknown request type: {type(request_o)}")
 
 # For text generation models
-def start_t2t(system_prompt: str, user_input: str) -> str:
+async def start_t2t(request_iterator):
     global t2t_model
-    for content_chunk in t2t_model(system_prompt,user_input):
+    system_input, user_input = "", ""
+    async for system_input_chunk, user_input_chunk in request_unpacker(request_iterator): # receiving chunks of info through a stream
+        system_input += system_input_chunk
+        user_input += user_input_chunk
+    for content_chunk in t2t_model(system_input,user_input):
         yield content_chunk
 
+# For speech-to-text models
+async def start_stt(request_iterator) -> str:
+    for audio, sample_rate, sample_width, channels in request_unpacker(request_iterator): # receiving chunks of info through a stream
+        raise NotImplementedError
+
 # For text-to-speech generation
-def start_ttsg(text: str) -> bytes:
-    raise NotImplementedError
+async def start_ttsg(request_iterator) -> str:
+    for text in request_unpacker(request_iterator): # receiving chunks of info through a stream
+        raise NotImplementedError
 
 # For voice changers
-def start_ttsc(audio: bytes) -> bytes:
-    raise NotImplementedError
+async def start_ttsc(request_iterator) -> str:
+    for audio, sample_rate, sample_width, channels in request_unpacker(request_iterator): # receiving chunks of info through a stream
+        raise NotImplementedError
